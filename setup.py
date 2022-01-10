81661e45c66e69b63934f1bb9faa8ebc061bd833
bistcuite <85484922+bistcuite@users.noreply.github.com>
2022-01-10 08:38:38 +0330

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
     
 setup (
     name='eitaa_pykit',
-    version='1.2',
+    version='1.2.1',
     packages=['eitaa'],
     long_description=long_description,
     long_description_content_type="text/markdown",
