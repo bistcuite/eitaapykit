bafabc4ce7e188b49b95da5c302250a695940fcc
bistcuite <85484922+bistcuite@users.noreply.github.com>
2022-09-01 00:17:29 +0430

    add get_info method

--- eitaa/__init__.py
+++ eitaa/__init__.py
@@ -8,6 +8,26 @@ class Eitaa(object):
     def __init__(self, token):
         self.token = token
 
+    @staticmethod
+    def get_info(channel_id):
+        r = requests.get(f"https://eitaa.com/{channel_id}")
+        soup = BeautifulSoup(r.text, 'html.parser')
+                
+        channel_name = soup.find('div', attrs = {'class':'tgme_page_title'}).text
+            
+        channel_image_url = "https://eitaa.com" + soup.find('img', attrs = {'class':'tgme_page_photo_image'})['src'] 
+
+        users_count = (str(soup.find('div', attrs = {'style':'display: block;text-align: center;font-weight: bold'}).text).split(' '))[0]
+        desc = soup.find('div', attrs = {'class':'text-center'}).text 
+
+        result = {
+            'name' : " ".join(channel_name.split()),
+            'image_url' : channel_image_url,
+            'users' : users_count,
+            'desc' : desc,
+        }
+        return result
+        
     def send_message(self, chat_id, text, pin=False, view_delete=-1):
         r = requests.post(
             f"https://eitaayar.ir/api/{self.token}/sendMessage",
