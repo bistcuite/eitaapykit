36af96837d2fbc3531231a450ba77777059063a4
bistcuite <85484922+bistcuite@users.noreply.github.com>
2023-03-15 19:58:05 +0330

    publish new version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
     
 setup (
     name='eitaa',
-    version='1.5.0',
+    version='2.0.0',
     packages=['eitaa'],
     long_description=long_description,
     long_description_content_type="text/markdown",
