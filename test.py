98b212d28ce88b147473a4fdeda866db96681229
Hasan <85484922+bistcuite@users.noreply.github.com>
2025-12-08 12:39:52 +0330

    new export

---
+++ test.py
@@ -0,0 +1,12 @@
+from eitaa import Eitaa
+
+e = Eitaa(token="bot7998:f08db347-74d3-4a50-bde9-f464ce11f73f")
+# print(e.get_info("@eitaa"))
+# print(e.get_latest_messages("@eitaa"))
+# from time import sleep
+# sleep(2000)
+# print(e.get_trends())
+# print(e.get_message("@eitaa",200))
+# print(e.send_message("9486296","سلام",True))
+# print(e.send_file("9486296","سلاملکم","./test.py",True))
+print(e.delete_message("9486296","2940"))
